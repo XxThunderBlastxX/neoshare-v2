585ae238767ef517aaa3c49507c556bb934bfc72
XxThunderBlastxX <koustavmondal55@gmail.com>
2024-12-22 22:10:36 +0530

    Added login ui

--- ui/app/routes/_index/route.tsx
+++ ui/app/routes/_index/route.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import {Link} from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,7 +11,11 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
       <section className="px-8 pt-8">
-        <button className="btn btn-outline btn-success mb-8">Login</button>
+          <Link to={"/login"}>
+            <button className="btn btn-outline btn-success mb-8">
+                Login
+            </button>
+          </Link>
         <h1 className="pb-3">NeoShare </h1>
         <p className="pb-3">This is a simple file sharing service. Upload your files and share them with others.</p>
         <p className="pb-3">On NeoShare, you can simply upload files. You can share links to those files with anyone, and they never have to sign up for an account.</p>
---
+++ ui/app/service/pocketbase.ts
@@ -0,0 +1,5 @@
+import PocketBase from "pocketbase";
+
+const pb : PocketBase = new PocketBase('');
+
+export default pb;
